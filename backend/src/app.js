473a2090f2d206b7f58483ad466eb4d52f971726
waquaralipatel <162323075+waquaralipatel@users.noreply.github.com>
2025-12-25 17:10:41 +0530

    Create app.js

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+const articleRoutes = require("./routes/articleRoutes");
+const scrapeOldBlogs = require("./scraper/scrapeOldBlogs");
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", articleRoutes);
+
+// Run scraper once at startup (optional)
+scrapeOldBlogs();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
